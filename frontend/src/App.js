4bb7aadf447a5bde1b723f6940de48d5d4b72d6f
Roganovic Radivoje <roganovic@dr.com>
2026-02-15 15:25:52 +0100

    Add catch-all route for GitHub Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import "@/App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        {/* din vanliga startsida på "/" */}
         <Route path="/" element={<HomePage />} />
+
+        {/* fånga ALLA andra paths (inkl. /Ambulanta-Care-v1/) och skicka till "/" */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </div>
   );
